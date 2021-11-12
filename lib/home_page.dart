9cce57a5bca3e5cffc963bb68627f1234eb70c54
SUYOG PATIL <suyog732002@gmail.com>
2021-11-12 18:19:34 +0530

    Final Touch

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
@@ -26,7 +27,6 @@ class _HomePageState extends State<HomePage> {
         listData = json.decode(response.body);
       });
     }
-    return listData;
   }
 
   @override
@@ -39,12 +39,14 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFF424242),
-      body: listData == null
-          ? const CircularProgressIndicator()
-          : RefreshIndicator(
-              onRefresh: () => apiCall(),
+    return FutureBuilder(
+      future: http.get(Uri.parse(apiUrl)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            backgroundColor: const Color(0xFF424242),
+            body: RefreshIndicator(
+              onRefresh: () => http.get(Uri.parse(apiUrl)),
               color: Colors.white,
               backgroundColor: Colors.black45,
               child: PageView.builder(
@@ -55,6 +57,16 @@ class _HomePageState extends State<HomePage> {
                 itemCount: listData.length,
               ),
             ),
+          );
+        } else {
+          return Container(
+            color: const Color(0xFF424242),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: const Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
     );
   }
 }
